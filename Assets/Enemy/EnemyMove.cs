db7c87daaca31ee3eac5a59abe5cc56b0b243d76
Joe Simoes <joe@generalassemb.ly>
2014-10-15 17:49:31 -0400

    Enemy moving

---
+++ Assets/Enemy/EnemyMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMove : MonoBehaviour {
+
+	public float MoveSpeed = 5f;
+	public float WaitTime = 2f;
+
+	private float TimeCreated;
+	private bool startedMove = false;
+	// Use this for initialization
+	void Start () {
+		TimeCreated = Time.time;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Time.time - TimeCreated > WaitTime) {
+			startedMove = true;
+			rigidbody.velocity = new Vector3(-MoveSpeed, 0, 0);
+		}
+	}
+}
---
+++ Assets/Enemy/Manager/ZombieSpawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieSpawner : MonoBehaviour {
+	
+	public float SpawnTimer = 2f;
+	public GameObject zombie_prefab;
+	public GameObject spawner1;
+	public GameObject spawner2;
+
+	private float lastSpawnTime;
+	private int lastSpawnLoc = 2;
+
+	// Use this for initialization
+	void Start () {
+		lastSpawnTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - lastSpawnTime > SpawnTimer) {
+			lastSpawnTime = Time.time;
+			Vector3 spawnPos;
+			if (lastSpawnLoc == 2) {
+				spawnPos = spawner1.transform.position;
+				lastSpawnLoc = 1;
+			} else {
+				spawnPos = spawner2.transform.position;
+				lastSpawnLoc = 2;
+			}
+			GameObject zombie = Instantiate(zombie_prefab, spawnPos, Quaternion.identity) as GameObject;
+			zombie.GetComponent<Animator>().SetInteger("randomIdle",Random.Range (1,2));
+			zombie.GetComponent<Animator>().SetInteger("randomMove",Random.Range (1,6));
+			zombie.transform.Rotate(0,-90,0);
+		}
+	}
+}
